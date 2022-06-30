3c102610df2951b151fdb711365f154563240d4e
artemZholus <artem.zholus@gmail.com>
2022-06-30 13:34:49 +0200

    fix view.py

--- gridworld/view.py
+++ gridworld/view.py
@@ -1,11 +1,12 @@
 from gridworld.viewer import Viewer
 from gridworld.render import setup
+from functools import partial
 import pyglet
 
 if __name__ == '__main__':
-    viewer = Viewer(width=800, height=600, 
+    viewer = Viewer(width=800, height=600, overlay=True, 
                     caption='Pyglet', resizable=True)
-    pyglet.clock.schedule_interval(viewer.agent.update, 1.0 / 200)
+    pyglet.clock.schedule_interval(partial(viewer.world.update, viewer.agent), 1.0 / 200)
     viewer.set_exclusive_mouse(True)
     setup()
     pyglet.app.run()
