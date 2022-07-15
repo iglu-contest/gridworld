fd658658606295e10e07d04e153e9a9008762399
artemZholus <artem.zholus@gmail.com>
2022-07-15 22:16:24 +0300

    visualizer

--- gridworld/visualizer.py
+++ gridworld/visualizer.py
@@ -67,44 +67,20 @@ class Visualizer:
         return self.renderer.render()[..., :-1]
 
     def render_video(self, output, 
-            event_sequence, init_conds=None, on_env_step=None,
-            render_text=False, text=None, render_crosshair=False, 
-            render_debug=False, reset=True, close=True, writer=None,
+            event_sequence, init_conds=None,
+            overlay=False, render_debug=False, close=True,
             verbose=False, fps=60
         ):
         import cv2
-        # TODO: set current task for the env
         if init_conds is not None:
             self.set_agent_state(*init_conds)
-        done = False
-        if writer is None:
-            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-            output.parent.mkdir(exist_ok=True)
-            writer = cv2.VideoWriter(f'{output}.mp4', fourcc, fps, self.render_size)
-            # if reset:
-                # image = self.postprocess_frame(
-                #     obs['pov'][:], render_text=render_text, render_crosshair=render_crosshair,
-                #     render_debug=render_debug, text=text,
-                #     g=None, n=None, last_action='None'
-                # )
-                # if callable(on_env_step):
-                #     on_env_step(None, -1, 0, output, obs, 0, done, None)
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        output.parent.mkdir(exist_ok=True)
+        writer = cv2.VideoWriter(f'{output}.mp4', fourcc, fps, self.render_size)
         image = self.render()
-        writer.write(np.transpose(image, (0, 1, 2))[..., ::-1])
+        writer.write(image[..., ::-1])
         tq = tqdm(total=len(event_sequence), disable=not verbose)
-        last_key_action = 'None'
-        for i, event in enumerate(event_sequence):
-            # if event.kind == 'action':
-            #     last_key_action = event.params[0]
-            # for k, action in enumerate(event.actions):
-                # obs, reward, done, info = self.env.step(action)
-                # image = self.postprocess_frame(
-                #     obs['pov'][:], render_text=render_text, render_crosshair=render_crosshair,
-                #     render_debug=render_debug, text=text,
-                #     g=event.turn, n=event.step, last_action=last_key_action
-                # )
-                # if callable(on_env_step):
-                #     on_env_step(event, i, k, output, obs, reward, done, info)
+        for _, event in enumerate(event_sequence):
             if event.kind != 'action':
                 self.set_agent_state(position=event.position.tolist(), 
                                      rotation=event.camera.tolist())
@@ -117,8 +93,6 @@ class Visualizer:
         tq.close()
         if close:
             writer.release()
-            # self.env.close()
-            # self.postproc_video(output)
         else:
             return writer
 
