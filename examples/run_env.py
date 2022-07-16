4f4826d16d5874eb89adae4fb0ff76e8cc937daf
artemZholus <artem.zholus@gmail.com>
2022-07-16 18:54:30 +0300

    fix run_env.py

--- examples/run_env.py
+++ examples/run_env.py
@@ -5,24 +5,23 @@ import gridworld
 from gridworld.tasks import DUMMY_TASK
 from time import perf_counter
 
-# create vector based env. Rendering is enabled by default. 
+# create vector based env. Rendering is enabled by default.
 # To turn it off, use render=False.
-# Note that without 
-env = gym.make('IGLUGridworldVector-v0')
+# Note that without
+env = gym.make('IGLUGridworld-v0')
 
 env.set_task(DUMMY_TASK)
 print(f'Action space: {env.action_space}')
 # print(f'Observation space: {env.observation_space}')
 time = 0
 steps = 0
-for ep in range(500):
+for ep in range(100):
     done = False
     obs = env.reset()
     while not done:
         action = env.action_space.sample()
         t = perf_counter()
         obs, reward, done, info = env.step(action)
-        print(obs["grid"].shape, reward)
         time += perf_counter() - t
         steps += 1
 print(f'steps per second: {steps / time:.4f}')
