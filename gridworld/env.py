67b617ad3e33d4a9946534b76ee7960c27d03ea0
Zoya Volovikova <zvolovikova@cds2.isa.ru>
2022-07-21 16:26:41 +0300

    commett render lines

--- gridworld/env.py
+++ gridworld/env.py
@@ -3,7 +3,7 @@ import pyglet
 pyglet.options["headless"] = True
 from gridworld.world import World
 from gridworld.control import Agent
-from gridworld.render import Renderer, setup
+#from gridworld.render import Renderer, setup
 from gridworld.task import Task
 
 from gym.spaces import Dict, Box, Discrete
